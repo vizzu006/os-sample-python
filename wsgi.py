791b67e1a4d78f8a521f82bfff6716c6f3165479
Vijay Mohan Boddu <38586210+vizzu006@users.noreply.github.com>
2018-10-11 07:53:00 +0530

    message edited

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World!This is VJ testing Openshift :)"
 
 if __name__ == "__main__":
     application.run()
